80348903a420cea0e38aa86a7e4d009559a8da80
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-03-24 20:54:12 +0200

    added a top list

--- TrafficSim/TrafficSim.cs
+++ TrafficSim/TrafficSim.cs
@@ -1,4 +1,6 @@
 using Jypeli;
+using Jypeli.Widgets;
+
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -17,6 +19,7 @@ public class TrafficSim : PhysicsGame
     private Progress _progress;
     private Player _car;
     private RoadMap _roadMap;
+    private TopList _topList;
     public bool DebugEnabled = false;
     public override void Begin()
     {
@@ -29,10 +32,12 @@ public class TrafficSim : PhysicsGame
         ClearAll();
         Keyboard.Listen(Key.R, ButtonState.Pressed, Init, "");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
-        
-        _progress = new Progress(this, RandomGen.NextDouble(10000, 40000));
+        _progress = new Progress(this, 6000);
         _car = new Player(this);
         _roadMap = new RoadMap(this, _progress);
+
+        _topList = new TopList(this);
+        
         Debug.Start(this, _car, _roadMap);
         MessageDisplay.Add("Press SPACE to begin!");
         Keyboard.Listen(Key.Space, ButtonState.Down, _progress.StartGame, "");
@@ -48,17 +53,16 @@ public class TrafficSim : PhysicsGame
     {
         IsPaused = true;
         var elapsedTime = _progress.StopTimer();
-        Console.WriteLine(elapsedTime);
         CreateSelectionWindow(elapsedTime);
         RemoveCollisionHandlers();
     }
-
-    private void CreateSelectionWindow(double finishTime)
+    
+    public void CreateSelectionWindow(double finishTime)
     {
         string[] options = { "Top List", "Restart", "Quit"};
         var endWindow = new MultiSelectWindow($"Finished in! {finishTime}", options);
         
-        endWindow.AddItemHandler(0, delegate{});
+        endWindow.AddItemHandler(0, delegate { _topList.ShowTopList(finishTime); });
         endWindow.AddItemHandler(1, Init);
         endWindow.AddItemHandler(2, Exit);
         
